682f506d9005c787a6f63a37c79ce2bd08537d91
Leon <khapib1025@gmail.com>
2023-02-10 11:11:57 +0800

    MediaAcceptance【版權採購交片單】移除 驗收明細 的 DTL_MASTERING_GROUP_ID【[母帶]群組ID】。

--- OA_WEB_API/Models/BPMPro/MediaAcceptanceModel.cs
+++ OA_WEB_API/Models/BPMPro/MediaAcceptanceModel.cs
@@ -136,9 +136,6 @@ namespace OA_WEB_API.Models.BPMPro
         /// <summary>母帶受領日期</summary>
         public Nullable<DateTime> DTL_GET_MASTERING_DATE { get; set; }
 
-        /// <summary>[母帶]群組ID</summary>
-        public string DTL_MASTERING_GROUP_ID { get; set; }
-
         /// <summary>驗收負責人主要部門</summary>
         public string DTL_OWNER_DEPT_MAIN_ID { get; set; }
 

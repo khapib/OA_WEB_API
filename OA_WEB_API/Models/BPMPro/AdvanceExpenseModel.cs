8874011035c889f129dec7f9198006f7c49a2760
Leon <khapib1025@gmail.com>
2024-01-09 16:22:49 +0800

    1. SMS【簡訊】發送問題 AdvanceExpenseRepository【預支費用申請單】 暫停調整。

--- OA_WEB_API/Models/BPMPro/AdvanceExpenseModel.cs
+++ OA_WEB_API/Models/BPMPro/AdvanceExpenseModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Docker.DotNet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Profile;
 
 namespace OA_WEB_API.Models.BPMPro
 {
@@ -28,6 +30,9 @@ namespace OA_WEB_API.Models.BPMPro
         /// <summary>預支費用申請單 表單內容 設定</summary>
         public AdvanceExpenseConfig ADVANCE_EXPENSE_CONFIG { get; set; }
 
+        /// <summary>預支費用申請單 預知明細 設定</summary>
+        public AdvanceExpenseDetailsConfig ADVANCE_EXPENSE_DETAILS_CONFIG { get; set; }
+
         /// <summary>表單關聯</summary>
         public IList<AssociatedFormConfig> ASSOCIATED_FORM_CONFIG { get; set; }
 
@@ -43,30 +48,12 @@ namespace OA_WEB_API.Models.BPMPro
     }
 
     /// <summary>
-    /// 費用申請單 表單內容 設定
+    /// 預支費用申請單 表單內容 設定
     /// </summary>
-    public class AdvanceExpenseConfig : DOM_TWD_Bank
+    public class AdvanceExpenseConfig
     {
-        /// <summary>預支員工部門編號</summary>
-        public string ADV_STAFF_DEPT_ID { get; set; }
-
-        /// <summary>預支員工部門名稱</summary>
-        public string ADV_STAFF_DEPT_NAME { get; set; }
-
-        /// <summary>預支員工編號</summary>
-        public string ADV_STAFF_ID { get; set; }
-
-        /// <summary>預支員工姓名</summary>
-        public string ADV_STAFF_NAME { get; set; }
-
-        /// <summary>
-        /// 支付方式：
-        /// CS.現金
-        /// PT_AC.薪轉帳戶
-        /// OR.其他
-        /// DT.(國內)_台幣其他帳戶(需自行負擔手續費)
-        /// </summary>
-        public string PAY_METHOD { get; set; }
+        /// <summary>是否過協理</summary>
+        public string IS_CFO { get; set; }
 
         /// <summary>申請源由</summary>
         public string REASON { get; set; }
@@ -74,21 +61,8 @@ namespace OA_WEB_API.Models.BPMPro
         /// <summary>備註</summary>
         public string NOTE { get; set; }
 
-        /// <summary>預支金額</summary>
-        public int ADVANCE_AMOUNT { get; set; }
-
-        /// <summary>預支日期</summary>
-        public Nullable<DateTime> ADVANCE_DATE { get; set; }
-
-        /// <summary>預計還款沖銷日</summary>
-        public Nullable<DateTime> REPYMT_DATE { get; set; }
-
-        /// <summary>
-        /// 還款方式：
-        /// CS.現金
-        /// TM.轉帳
-        /// </summary>
-        public string REPYMT_TYPE { get; set; }
+        /// <summary>換算台幣 合計</summary>
+        public int AMOUNT_CONV_TOTAL { get; set; }
 
         /// <summary>財務審核人員編號</summary>
         public string FINANC_AUDIT_ID_1 { get; set; }
@@ -103,5 +77,55 @@ namespace OA_WEB_API.Models.BPMPro
         public string FINANC_AUDIT_NAME_2 { get; set; }
     }
 
+    /// <summary>
+    /// 預支費用申請單 預知明細 設定
+    /// </summary>
+    public class AdvanceExpenseDetailsConfig : COMM_Bank
+    {
+        /// <summary>行數編號</summary>
+        public int ROW_NO { get; set; }
 
+        /// <summary>預知幣別</summary>
+        public string ADVANCE_CURRENCY_NAME { get; set; }
+
+        /// <summary>匯率</summary>
+        public float EXCHANGE_RATE { get; set; }
+
+        /// <summary>預支金額</summary>
+        public float ADVANCE_AMOUNT { get; set; }
+
+        /// <summary>換算台幣</summary>
+        public int Amount_CONV { get; set; }
+
+        /// <summary>預支日期</summary>
+        public DateTime ADVANCE_DATE { get; set; }
+
+        /// <summary>預計還款沖銷日</summary>
+        public DateTime REPAYMENT_DATE { get; set; }
+
+        /// <summary>
+        /// 還款方式：
+        /// CS.現金、
+        /// TM.轉帳
+        /// </summary>
+        public string REPAYMENT_TYPE { get; set; }
+
+        /// <summary>備註</summary>
+        public string NOTE { get; set; }
+
+        /// <summary>
+        /// 支付方式：
+        /// CS.現金
+        /// PT_AC.薪轉帳戶
+        /// OR.其他
+        /// DT.其他帳戶(需自行負擔手續費)
+        /// </summary>
+        public string PAY_METHOD { get; set; }
+
+        /// <summary>帳戶類別</summary>
+        public string ACCOUNT_CATEGORY { get; set; }
+
+        /// <summary>支付對象</summary>
+        public string PAYMENT_OBJECT { get; set; }
+    }
 }

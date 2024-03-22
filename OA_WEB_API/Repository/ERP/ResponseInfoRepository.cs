386bb2ae0aa611e339ffb91cc8c8a11dabd55adf
Leon <khapib1025@gmail.com>
2024-03-22 15:16:27 +0800

    發布 ： 1. 【回傳ERP資訊】 企業乘車對帳單 對帳資訊_回傳ERP 寫入ERP回傳路徑。

--- OA_WEB_API/Repository/ERP/ResponseInfoRepository.cs
+++ OA_WEB_API/Repository/ERP/ResponseInfoRepository.cs
@@ -280,7 +280,7 @@ namespace OA_WEB_API.Repository.ERP
 
                 if (query.REQUEST_FLG)
                 {
-                    ApiUrl = GlobalParameters.ERPSystemAPI(GlobalParameters.sqlConnBPMProDevHo) + "BPM/";
+                    ApiUrl = GlobalParameters.ERPSystemAPI(GlobalParameters.sqlConnBPMProDevHo) + "BPM/UpdateEefCtrs_DetailContent";
                     Method = "POST";
                     strResponseJson = GlobalParameters.RequestInfoWebAPI(ApiUrl, Method, enterpriseTaxiReviewInfoRequest);
 
